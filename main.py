452fcbfcabd7873a740615d93aa4a5abc7c006df
Gontafi <106583228+Gontafi@users.noreply.github.com>
2022-12-19 23:34:31 +0600

    fixed

--- main.py
+++ main.py
@@ -3,16 +3,18 @@ from models import *
 person = Person(age=18, full_name='Alibek')
 driver = Driver(experience=5, age=25, full_name='John Doe')
 engine = Engine(power=250, company='Mercedes')
-car = Car(carClass='B', driver=driver, engine=engine, marka='Mercedes')
+car = Car(carClass='B', driver=driver, engine=engine, marka='Mercedes', weight=450)
 lorry = Lorry(carClass='D',
               engine=Engine(power=800, company='Kamaz'),
               driver=Driver(age=35, full_name='Tolebekov Dauren', experience=8),
               marka='Kamaz',
+              weight=1400,
               carrying=1500)
 sport_car = SportCar(carClass='B',
                      engine=Engine(power=980, company='Nissan'),
                      driver=Driver(age=36, full_name='Scott Pruett',experience=10),
                      marka='BMW',
+                     weight=700,
                      speed=450.5)
 
 print(f'Person: {person}')
